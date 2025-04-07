f466357b5501c0887d9b4619a9e3171663e3fc74
nvk <rodolfo@rnvk.org>
2025-04-07 13:59:09 -0400

    Bump version to 1.0.1

--- webdownloader.rb
+++ webdownloader.rb
@@ -6,7 +6,7 @@ class Webdownloader < Formula
 
   desc "Command-line tool to download websites for offline use with multiple output options"
   homepage "https://github.com/nvk/webdownloader"
-  url "https://github.com/nvk/webdownloader/archive/refs/tags/v1.0.0.tar.gz"
+  url "https://github.com/nvk/webdownloader/archive/refs/tags/v1.0.1.tar.gz"
   sha256 "0969a8d1a97900ac880f641d39fe79cdf23e3a37db4d0aa7328005b62efdadf8"
   license "MIT"
 
